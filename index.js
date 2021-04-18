dfdffdb658003f55f4cbab338b6bf408112f804a
Troinov Mikhail <40204735+trnvmkhl@users.noreply.github.com>
2021-04-18 15:13:54 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+function myFunction() {
+  let popup = document.querySelector(".popup-div");
+  popup.classList.toggle("show");
+  document.querySelector('path').classList.toggle('share-arrow');
+  document.querySelector('.btn').classList.toggle('toggled-btn');
+}
